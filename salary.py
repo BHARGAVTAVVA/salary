3bb34b1839b032ea23e38f480306a2009b0183c8
BHARGAVTAVVA <111734292+BHARGAVTAVVA@users.noreply.github.com>
2022-09-02 12:10:58 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 pickle_in=open('Salaryprediction.pkl','rb')
 model=pickle.load(pickle_in
-e=st.number_input ('enter exp')
+e = st.number_input ('enter exp')
 if st.button('predict'):
   r=model.predict([[e]]).squeeze()
   st.success(f'Salary is {r}')
