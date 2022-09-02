134118533a958d9a43672ced504f38be39132f94
BHARGAVTAVVA <111734292+BHARGAVTAVVA@users.noreply.github.com>
2022-09-02 12:09:58 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -4,6 +4,6 @@ pickle_in=open('Salaryprediction.pkl','rb')
 model=pickle.load(pickle_in
 e=st.number_input ('enter exp')
 if st.button('predict'):
-                  s=model.predict([[e]]).squeeze()
-                  st.success(f'Salary is {s}')
-  
+  r=model.predict([[e]]).squeeze()
+  st.success(f'Salary is {r}')
+                  
